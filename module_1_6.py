17266b3eff7c28aa21d03551082b92f6af7ec1e8
KimS675 <89504121717el@gmail.com>
2024-12-04 10:59:47 +0700

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Dasha' : 1992, 'Masha' : 1999, 'Glasha' : 2003}
+print(my_dict)
+print(my_dict.get('Dasha'))
+print(my_dict.get('Sasha'))
+my_dict.update({'Lera': 2001, 'Sveta': 2005})
+a = my_dict.pop ('Masha')
+print(a)
+print(my_dict)
+
+my_set = {1,2,1,2,3,4,5,1,22}
+print(my_set)
+my_set.update({7, 8})
+my_set.discard(1)
+print(my_set)
+
