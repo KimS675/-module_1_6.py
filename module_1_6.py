6de22fe7c2f75a398baabfc65d2acda7afe423d1
KimS675 <89504121717el@gmail.com>
2024-12-04 11:19:42 +0700

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,15 +1,15 @@
 my_dict = {'Dasha' : 1992, 'Masha' : 1999, 'Glasha' : 2003}
-print(my_dict)
-print(my_dict.get('Dasha'))
-print(my_dict.get('Sasha'))
+print('Список людей и их годов рождения: ', my_dict)
+print('Существующее значение: ', my_dict.get('Dasha'))
+print('Несуществующее значение: ', my_dict.get('Sasha'))
 my_dict.update({'Lera': 2001, 'Sveta': 2005})
 a = my_dict.pop ('Masha')
-print(a)
-print(my_dict)
+print("Удаленное значение: ", a)
+print("Измененный список людей и их годов рождения: ", my_dict)
 
 my_set = {1,2,1,2,3,4,5,1,22, 'law', (1, 2, 3) }
-print(my_set)
+print("Множество: ", my_set)
 my_set.update({7, 65, 'fgd'})
 my_set.discard(1)
-print(my_set)
+print("Измененное множество: ", my_set)
 
